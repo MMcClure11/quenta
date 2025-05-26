53ec337067ae60e656dd5bfac289178cfd5bc90d
MMcClure11 <mmcclure0100@gmail.com>
2025-05-26 14:24:48 +0200

    feat: add creation of ExpenseItem to Expense feature

--- elixir/quenta/lib/quenta_web/expense_live/new.ex
+++ elixir/quenta/lib/quenta_web/expense_live/new.ex
@@ -1,15 +1,13 @@
 defmodule QuentaWeb.ExpenseLive.New do
   use QuentaWeb, :live_view
-  alias Quenta.Expenses.Expense
+
   alias Quenta.Expenses
+  alias Quenta.Expenses.Expense
   alias Quenta.Users
 
   def mount(_params, _session, socket) do
-    form = build_form(%{})
-
-    user_options =
-      Users.list_users()
-      |> Enum.map(fn user -> {user.name, user.id} end)
+    form = %Expense{} |> Expenses.change_expense(%{}) |> to_form()
+    user_options = Users.list_users() |> Enum.map(fn user -> {user.name, user.id} end)
 
     socket =
       socket
@@ -19,31 +17,13 @@ defmodule QuentaWeb.ExpenseLive.New do
     {:ok, socket}
   end
 
-  def handle_event("validate", params, socket) do
-    %{"expense" => attrs} = params
-
-    # Convert amount from string to integer (cents)
-    %{"amount_cents" => amount_dollars} = attrs
-    amount_cents = price_to_cents(amount_dollars)
-    attrs = Map.put(attrs, "amount_cents", amount_cents)
-
-    form =
-      %Expense{}
-      |> Expenses.change_expense(attrs)
-      |> to_form(action: :validate)
-
+  def handle_event("validate", %{"expense" => expense_attrs}, socket) do
+    form = %Expense{} |> Expenses.change_expense(expense_attrs) |> to_form(action: :validate)
     {:noreply, assign(socket, :form, form)}
   end
 
-  def handle_event("save", params, socket) do
-    %{"expense" => attrs} = params
-
-    # Convert amount from string to integer (cents)
-    %{"amount_cents" => amount_dollars} = attrs
-    amount_cents = price_to_cents(amount_dollars)
-    attrs = Map.put(attrs, "amount_cents", amount_cents)
-
-    case Expenses.create_expense(attrs) do
+  def handle_event("save", %{"expense" => expense_attrs}, socket) do
+    case Expenses.create_expense(expense_attrs) do
       {:ok, _} ->
         {:noreply,
          socket
@@ -58,37 +38,60 @@ defmodule QuentaWeb.ExpenseLive.New do
   def render(assigns) do
     ~H"""
     <.form for={@form} phx-change="validate" phx-submit="save">
-      <.input label="Description" type="text" field={@form[:description]} />
-      <.input label="Amount" type="number" field={@form[:amount_cents]} step="0.01" />
-      <.input label="Date" type="date" field={@form[:date]} />
-      <.input
-        label="Paid By"
-        type="select"
-        field={@form[:user_id]}
-        options={@user_options}
-        prompt="Select User"
-      />
-      <button>Save</button>
+      <div class="flex flex-col gap-3">
+        <.input label="Description" type="text" field={@form[:description]} />
+        <.input label="Amount" type="number" field={@form[:amount_dollars]} step="0.01" />
+        <.input
+          label="Date"
+          type="date"
+          field={@form[:date]}
+          value={Date.utc_today() |> Date.to_iso8601()}
+        />
+        <.input label="Paid By" type="select" field={@form[:user_id]} options={@user_options} />
+      </div>
+
+      <h3 class="font-lg font-bold mt-6 mb-3">Expense Items</h3>
+      <.inputs_for :let={eif} field={@form[:expense_items]}>
+        <input type="hidden" name="expense[expense_items_sort][]" value={eif.index} />
+        <div class="flex flex-row gap-3 w-full border-2 p-4 my-2 rounded-md">
+          <div class="flex flex-col gap-3 grow">
+            <.input label="Description" type="text" field={eif[:description]} />
+            <.input label="Amount" type="number" field={eif[:amount_dollars]} step="0.01" />
+            <.input label="Bought By" type="select" field={eif[:user_id]} options={@user_options} />
+          </div>
+
+          <button
+            type="button"
+            name="expense[expense_items_drop][]"
+            value={eif.index}
+            phx-click={JS.dispatch("change")}
+            class="justify-start"
+          >
+            <.icon name="hero-x-mark" class="w-6 h-6 hover:text-red-500" />
+          </button>
+        </div>
+      </.inputs_for>
+
+      <input type="hidden" name="expense[expense_items_drop][]" />
+
+      <div class="flex flex-row gap-3 my-3">
+        <button
+          type="button"
+          name="expense[expense_items_sort][]"
+          value="new"
+          class="p-2 bg-blue-300 hover:bg-blue-400 rounded-md"
+          phx-click={JS.dispatch("change")}
+        >
+          Add Expense Item
+        </button>
+        <button
+          class="p-2 bg-blue-300 hover:bg-blue-400 disabled:bg-gray-400 rounded-md"
+          disabled={!@form.source.valid?}
+        >
+          Save
+        </button>
+      </div>
     </.form>
     """
-  end
-
-  defp build_form(attrs) do
-    to_form(Expenses.change_expense(%Expense{}, attrs))
-  end
-
-  defp price_to_cents("") do
-    ""
-  end
-
-  defp price_to_cents(price_string) do
-    float_val =
-      if String.contains?(price_string, ".") do
-        String.to_float(price_string)
-      else
-        String.to_integer(price_string) |> :erlang.float()
-      end
-
-    round(float_val * 100)
   end
 end
--- elixir/quenta/test/quenta_web/expense_live/new_test.exs
+++ elixir/quenta/test/quenta_web/expense_live/new_test.exs
@@ -19,7 +19,7 @@ defmodule QuentaWeb.ExpenseLive.NewTest do
     |> element("form")
     |> render_submit(%{
       expense: %{
-        amount_cents: "100.00",
+        amount_dollars: "100.00",
         description: "Test Expense",
         user_id: "1",
         date: "2023-10-01"
@@ -40,6 +40,59 @@ defmodule QuentaWeb.ExpenseLive.NewTest do
         description: "",
         user_id: "",
         date: ""
+      }
+    })
+
+    assert render(view) =~ "can&#39;t be blank"
+  end
+
+  test "creates a new expense with expense items", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/expenses/new")
+
+    view
+    |> element("form")
+    |> render_submit(%{
+      expense: %{
+        amount_dollars: "100.00",
+        description: "Test Expense",
+        user_id: "1",
+        date: "2023-10-01",
+        expense_items: %{
+          "0" => %{
+            description: "Item 1",
+            amount_dollars: "50.00",
+            user_id: "1"
+          },
+          "1" => %{
+            description: "Item 2",
+            amount_dollars: "30.00",
+            user_id: "2"
+          }
+        }
+      }
+    })
+
+    assert_redirected(view, ~p"/")
+  end
+
+  test "displays validation errors for expense items", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/expenses/new")
+
+    view
+    |> element("form")
+    |> render_submit(%{
+      expense: %{
+        amount_cents: "100.00",
+        description: "Test Expense",
+        user_id: "1",
+        date: "2023-10-01",
+        expense_items: %{
+          "0" => %{
+            description: "",
+            amount_dollars: "",
+            user_id: ""
+          }
+        }
       }
     })
 
